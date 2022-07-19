894bba96fb66affca20c5023ac406e0a8ee8db92
Harry Nguyen <harry.nguyen2@techvify.com.vn>
2022-07-19 17:30:53 +0700

    init remix

---
+++ app/models/post.server.ts
@@ -0,0 +1,21 @@
+import { prisma } from "~/db.server";
+export type { Post } from "@prisma/client";
+type Post = {
+  slug: string;
+  title: string;
+  markdown: string;
+};
+
+export async function getPosts(): Promise<Array<Post>> {
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({ data: post });
+}
